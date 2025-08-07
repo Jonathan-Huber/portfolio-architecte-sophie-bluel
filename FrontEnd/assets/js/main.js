4a9a5cdbaba091f106c3bab1aa8ed7fb2be54d6f
Jonathan <jonathan.mfm@gmail.com>
2025-08-07 12:08:03 +0200

    feat: basculer l'affichage des projets en dynamique

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -5,10 +5,34 @@ async function fetchWorks() {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const works = await response.json();
-    console.log(works);
+
+    displayWorks(works);
+
   } catch (error) {
     console.error("Erreur lors de la récupération des travaux de l'API :", error.message);
   }
+}
+
+function displayWorks(works) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+
+  works.forEach(work => {
+    const figure = document.createElement("figure");
+
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+
+  });
+
 }
 
 fetchWorks();
