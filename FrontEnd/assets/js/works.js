6867ac671bdb2b5d3596a436706357b260f3974e
Jonathan <jonathan.mfm@gmail.com>
2025-08-18 15:43:27 +0200

    feat: supprimer un projet via l'API lors du clic sur le bouton delete

--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -1,5 +1,7 @@
 // WORKS.JS
 
+import { deleteWorkAPI } from "./api.js";
+
 // Activer les boutons de suppression lors du click
 export function setupDeleteButtons() {
   const deleteButtons = document.querySelectorAll(".modal-deleteBtn")
@@ -8,6 +10,13 @@ export function setupDeleteButtons() {
   deleteButtons.forEach(button => {
     button.addEventListener("click", async () => {
     const workId = button.closest("figure").dataset.id;
+
+    try {
+      // Supprimer le works de l'API
+      await deleteWorkAPI(workId, token);
+    } catch (error) {
+      console.error("Erreur lors de la suppression :", error.message);
+    }
 
     console.log("Tu as cliqué sur le bouton delete du projet avec l'Id :", workId)
     });
